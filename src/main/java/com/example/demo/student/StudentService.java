59cdf035b11302ceea553edd6765d39ba671f433
ralphvw <114740626+ralphvw@users.noreply.github.com>
2023-03-10 16:11:28 +0000

    Update: Adding controllers

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,5 +19,33 @@ public class StudentService {
     }
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()) throw new IllegalStateException("email taken");
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+       boolean exists = studentRepository.existsById(studentId);
+       if(!exists) throw new IllegalStateException("Student with id " + studentId + " does not exist");
+       studentRepository.deleteById((studentId));
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String email, String name) {
+       Student student = studentRepository.findById((studentId))
+               .orElseThrow( ()-> new IllegalStateException("Student with id " + studentId + " does not exist"));
+       if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+           student.setName(name);
+       }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()) throw new IllegalStateException("email taken");
+            student.setEmail((email));
+        }
+
     }
 }
